7bbae2c76673c2b70417079ce5f868962982653c
Hunter Perrin <hperrin@gmail.com>
2017-11-01 18:00:57 -0700

    Detect file exists correctly.

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-if [ -f "web_data/vendor" ]; then
+if [ ! -d "web_data/vendor" ]; then
   cd ./web_data/
   composer install
   cd ..
 fi
 
-if [ -f "db_password.txt" ]; then
+if [ ! -f "db_password.txt" ]; then
   dd if=/dev/urandom bs=64 count=1 | base64 > ./db_password.txt
   dd if=/dev/urandom bs=64 count=1 | base64 > ./db_root_password.txt
 fi
