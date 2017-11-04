f2361d408daac2b6012265835d9137690312136b
Hunter Perrin <hperrin@gmail.com>
2017-11-04 15:03:44 -0700

    Switched to alpine linux in npm and composer scripts.

--- composer.sh
+++ composer.sh
@@ -9,7 +9,7 @@ else
   # No composer on the host, so run with Docker image.
   if docker run -it --rm -v $PWD/app:/app composer $*; then
     # Make sure the files are owned by the user.
-    docker run -it --rm -v $PWD/app:/app ubuntu chown -R $(id -u):$(id -g) /app/vendor /app/composer.lock
+    docker run -it --rm -v $PWD/app:/app alpine chown -R $(id -u):$(id -g) /app/vendor /app/composer.lock
   else
     echo "Failed to install PHP libraries."
     exit 1
