a1181b4091412c9f2d90f0a038c4df9b88de9538
Hunter Perrin <hperrin@gmail.com>
2018-06-22 17:59:52 -0700

    Update packages and fix page stall.

---
+++ app/src/Services/SleepyCacheService.js
@@ -0,0 +1,39 @@
+import {Nymph} from 'nymph-client';
+
+export class SleepyCacheService {
+  constructor (entityClass) {
+    const that = this;
+    this._getEntityData = Nymph.getEntityData;
+    this.entityDataCache = {};
+
+    Nymph.getEntityData = function (...args) {
+      if (args[0].class === entityClass.class && args[1].type === '&' && typeof args[1].guid === 'number') {
+        if (!that.entityDataCache[args[1].guid] || that.entityDataCache[args[1].guid].expire <= (new Date())) {
+          that.entityDataCache[args[1].guid] = {
+            promise: that._getEntityData.call(this, ...args),
+            expire: (new Date()) + 1000 * 60 * 60 * 2 // Expire after 2 hours.
+          };
+        }
+        return that.entityDataCache[args[1].guid].promise;
+      }
+      return that._getEntityData.call(this, ...args);
+    };
+
+    this.timeout = setTimeout(() => {
+      for (let k in this.entityDataCache) {
+        if (this.entityDataCache[k].expire <= (new Date())) {
+          delete this.entityDataCache[k];
+        }
+      }
+    }, 60000);
+  }
+
+  destroy () {
+    Nymph.getEntityData = this._getEntityData;
+    clearTimeout(this.timeout);
+  }
+
+  clear () {
+    this.entityDataCache = {};
+  }
+};
--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,22 @@
 import Container from './Container.html';
 import UserStore from './UserStore';
 import {Nymph} from 'nymph-client';
+import {User, Group} from 'tilmeld-client';
+import {SleepyCacheService} from './Services/SleepyCacheService';
+
+const sleepyUserCacheService = new SleepyCacheService(User);
+const sleepyGroupCacheService = new SleepyCacheService(Group);
 
 const store = new UserStore({
   todos: [],
   sort: 'name',
   archived: false
 });
+
+store.constructor.prototype.refreshAll = function () {
+  sleepyUserCacheService.clear();
+  sleepyGroupCacheService.clear();
+};
 
 const app = new Container({
   target: document.querySelector('main'),
