e24ae02ce8fb21b55e4a587109ce504a52ef00b0
Hunter Perrin <hperrin@gmail.com>
2019-05-28 18:06:37 -0700

    Better production settings and get ready for hot module reloading.

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,62 +1,48 @@
-const webpack = require('webpack');
 const path = require('path');
+const { HotModuleReplacementPlugin } = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const analyze = process.env.WEBPACK_ANALYZE === 'true';
 const devMode = process.env.NODE_ENV !== 'production';
+const hotMode = process.env.WEBPACK_HOT === 'true';
 
-module.exports = {
+const common = {
   mode: devMode ? 'development' : 'production',
   optimization: {
     minimizer: [
       new TerserPlugin({
         sourceMap: true,
+        extractComments: 'some',
+        terserOptions: {
+          ecma: 8,
+          compress: {
+            passes: 3,
+          },
+          mangle: {
+            module: true,
+          },
+          module: true,
+        },
       }),
     ],
   },
   // devtool: devMode && 'source-map',
   devtool: 'source-map',
-  entry: {
-    main: path.resolve(__dirname, 'src', 'index.js'),
-    showdown: path.resolve(__dirname, 'src', 'index.showdown.js'),
-    '../ServiceWorker': path.resolve(
-      __dirname,
-      'src',
-      'Workers',
-      'ServiceWorker.js',
-    ),
-    'Workers/ResizeImage': path.resolve(
-      __dirname,
-      'src',
-      'Workers',
-      'ResizeImage.js',
-    ),
-    'Workers/AESEncryption': path.resolve(
-      __dirname,
-      'src',
-      'Workers',
-      'AESEncryption.js',
-    ),
-    'Workers/RSAEncryption': path.resolve(
-      __dirname,
-      'src',
-      'Workers',
-      'RSAEncryption.js',
-    ),
-  },
   output: {
     path: path.resolve(__dirname),
     filename: 'dist/[name].js',
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'dist/[name].css',
-      chunkFilename: 'dist/[id].css',
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: devMode || JSON.stringify('production'),
-      },
-    }),
+    ...(devMode
+      ? []
+      : [
+          new MiniCssExtractPlugin({
+            filename: 'dist/[name].css',
+            chunkFilename: 'dist/[id].css',
+          }),
+        ]),
+    ...(analyze ? [new BundleAnalyzerPlugin()] : []),
   ],
   resolve: {
     extensions: [
@@ -87,6 +73,8 @@ module.exports = {
           options: {
             dev: devMode,
             emitCss: true,
+            // This will be enabled as soon as svelte-loader supports HMR for Svelte 3.
+            hotReload: devMode && false,
           },
         },
       },
@@ -119,3 +107,55 @@ module.exports = {
     ],
   },
 };
+
+module.exports = [
+  {
+    entry: {
+      main: path.resolve(__dirname, 'src', 'index.js'),
+    },
+    ...common,
+    plugins: [
+      ...common.plugins,
+      ...(hotMode ? [new HotModuleReplacementPlugin()] : []),
+    ],
+    devServer: {
+      open: true,
+      compress: true,
+      port: 8083,
+      proxy: {
+        '/rest.php': 'http://localhost:8080',
+        '/user/': 'http://localhost:8080',
+      },
+    },
+  },
+  {
+    entry: {
+      showdown: path.resolve(__dirname, 'src', 'index.showdown.js'),
+      '../ServiceWorker': path.resolve(
+        __dirname,
+        'src',
+        'Workers',
+        'ServiceWorker.js',
+      ),
+      'Workers/ResizeImage': path.resolve(
+        __dirname,
+        'src',
+        'Workers',
+        'ResizeImage.js',
+      ),
+      'Workers/AESEncryption': path.resolve(
+        __dirname,
+        'src',
+        'Workers',
+        'AESEncryption.js',
+      ),
+      'Workers/RSAEncryption': path.resolve(
+        __dirname,
+        'src',
+        'Workers',
+        'RSAEncryption.js',
+      ),
+    },
+    ...common,
+  },
+];
