6c54b5f5edb6619bd811b96f2b27b57d08acf54c
Hunter Perrin <hperrin@gmail.com>
2020-12-07 16:46:57 -0800

    Allow REST requests from any origin.

--- app/rest.php
+++ app/rest.php
@@ -2,6 +2,24 @@
 
 error_reporting(E_ALL);
 
+// Allow from any origin.
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+  header('Access-Control-Allow-Credentials: true');
+  header('Access-Control-Max-Age: 86400');
+}
+
+// Access-Control headers are received during OPTIONS requests.
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+    header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, HEAD, OPTIONS");
+  }
+
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+  }
+}
+
 /*
  * You don't need to edit below here. It is set up to work inside your Docker
  * container.
